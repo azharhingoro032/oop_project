28784d71051b31a6adb4448ee62fc85fb35a0a41
azharhingoro032 <166338049+azharhingoro032@users.noreply.github.com>
2024-07-12 15:55:23 -0700

    oop_project

---
+++ main.js
@@ -0,0 +1,51 @@
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+console.log(chalk.blue("============================================================"));
+console.log(chalk.blue("||                                                         ||"));
+console.log(chalk.blue("||               Welcome to My OOP Project                 ||"));
+console.log(chalk.blue("||                                                         ||"));
+console.log(chalk.blue("============================================================"));
+async function main() {
+    let exit = false;
+    let students = [];
+    while (!exit) {
+        let oop = await inquirer.prompt([
+            {
+                type: "list",
+                name: "OOP",
+                message: "Please select an option:",
+                choices: [
+                    { name: "Self-Reflection", value: "selfReflection" },
+                    { name: "Speak to a Student", value: "speakToStudent" },
+                    { name: "Exit", value: "exit" }
+                ]
+            }
+        ]);
+        if (oop.OOP === "selfReflection") {
+            console.log("Hello! I'm reflecting on myself.");
+            console.log("I am doing well and in great health.");
+        }
+        else if (oop.OOP === "speakToStudent") {
+            let student = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'studentName',
+                    message: 'Please enter the student name:',
+                }
+            ]);
+            students.push(student.studentName);
+            console.log(`Hello, I am ${student.studentName}.`);
+            console.log("Nice to meet you! How are you doing today?");
+            console.log("\n--- List of Students ---");
+            students.forEach((name, index) => {
+                console.log(`${index + 1}. ${name}`);
+            });
+        }
+        else if (oop.OOP === "exit") {
+            await new Promise(resolve => setTimeout(resolve, 1000));
+            exit = true;
+            console.log("Exiting the program. Goodbye!");
+        }
+    }
+}
+main();
